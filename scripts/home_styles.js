84c5fce486e234c8e5a0f3fbb3fc81ab1ca80ca9
Godwin VC <alfa.godwin.omega@gmail.com>
2017-05-30 20:35:14 +0530

    added new functionality to home_styles

--- scripts/home_styles.js
+++ scripts/home_styles.js
@@ -1,3 +1,17 @@
+window.userDetails = null;
+window.fonts = {
+    "A": "Monotype Corsiva",
+    "B": "Great Vibes",
+    "C": "Snell Roundhand",
+    "D": "Brush Script",
+    "E": "Sign Painter",
+    "F": "Edwardian Script",
+    "G": "Diana Script",
+    "H": "ALS Script",
+    "I": "Cac Champagne",
+    "J": "Times New Roman"
+}
+window.pickedFont = null;
 window.homeStyles = function (body) {
     var elm = body.find('.home-styles-container:eq(0)');
     var _top = Math.round(elm.offset().top) - $(window).scrollTop();
@@ -12,10 +26,13 @@ window.homeStyles = function (body) {
         "margin-top": ($('nav:eq(0)').height() + 15) + "px"
     })
 }
-window.homeStylesInit = function(){
+window.homeStylesInit = function () {
     $('.home-style-form:eq(0)').submit(openStyles);
     $('[type="password"]').blur(passcodeCheck);
     $('[type="password"]').focus(passcodeCheck);
+    $('.input-group').click(pickStyle);
+    $('[confirm-order]').click(confirmOrder);
+    // setTimeout(generateStyles(), 10);
 }
 
 window.openStyles = function (e) {
@@ -29,6 +46,7 @@ window.openStyles = function (e) {
         rawData.forEach(function (element) {
             data[element.name] = element.value;
         }, this);
+        window.userDetails = JSON.stringify(data);
         $.ajax({
             type: "POST",
             url: "./endpoints/send-style-mail.php",
@@ -69,7 +87,7 @@ window.passcodeCheck = function (e) {
     }
 }
 
-window.generateStyles =  function() {
+window.generateStyles = function () {
     $('.home-style-form:eq(0)').fadeOut(500);
     $('.home-styles-container').css('width', '95%');
     var currentWidth = null;
@@ -83,17 +101,59 @@ window.generateStyles =  function() {
             $('.samples-container').show();
             $('.sample-input input').focus(updateSample);
             $('.sample-input input').blur(updateSample);
+            $('.footer').show();
         }
     }, 50)
 }
 
-function updateSample(e){
+function updateSample(e) {
     var that = this;
-    var loop = setInterval(function(){
-        if(e.type == 'focus'){
+    var loop = setInterval(function () {
+        if (e.type == 'focus') {
             $('.sample-display input').val($(that).val());
-        }else if(e.type == 'blur'){
+        } else if (e.type == 'blur') {
             clearInterval(loop)
         }
-    },100);
+    }, 100);
+}
+
+function pickStyle() {
+    if ($(this).hasClass('active') == true) {
+        $(this).removeClass('active');
+        $('.btn-default').addClass('disabled');
+    } else {
+        $('.input-group').removeClass('active');
+        $(this).addClass('active');
+        $('.btn-default').removeClass('disabled');
+        window.pickedFont = window.fonts[$('.active b').text()];
+        console.log(pickedFont);
+    };
+}
+window.confirmOrder = function (e) {
+    if (!$(this).hasClass('disabled')) {
+        userDetails = JSON.parse(userDetails);
+        userDetails.pickedFont = "(" + $('.active b').text() + ") " + window.pickedFont;
+        console.log(userDetails);
+        $('[confirm-order]').addClass('disabled');
+        $('[confirm-order]').html('<i class="fa fa-spinner fa-lg fa-pulse fa-fw"></i> <b>Placing order...</b>');
+        $.ajax({
+            type: "POST",
+            url: "./endpoints/confirm-order.php",
+            dataType: 'text',
+            data: {
+                myData: JSON.stringify(userDetails)
+            },
+            complete: function (r) {
+                if (r.responseText == "ok") {
+                    $('[confirm-order]').addClass('btn-success');
+                    $('[confirm-order]').removeClass('btn-default');
+                    $('[confirm-order]').html('<i class="ion-ios-checkmark fa-lg"></i> <b>Order Placed</b>');
+                } else {
+                    $('[confirm-order]').removeClass('disabled');
+                    $('[confirm-order]').html('<b>Confirm Order</b>');
+                    alert('Someting went wrong! Please try again');
+                }
+            }
+        })
+    }
 }
